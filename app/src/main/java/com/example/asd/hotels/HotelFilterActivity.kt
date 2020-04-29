ebb1ea996803e1868b3e415a1cd9df7d1fc8e39f
Sandra Haas <sandra.michaela.haas@gmail.com>
2020-04-29 19:39:34 +0200

    HOTEL_007 Filter category merge fix

--- app/src/main/java/com/example/asd/hotels/HotelFilterActivity.kt
+++ app/src/main/java/com/example/asd/hotels/HotelFilterActivity.kt
@@ -1,9 +1,9 @@
 package com.example.asd.hotels
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.SeekBar
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_seek_bar.*
 
 class SeekBarActivity : AppCompatActivity() {
@@ -12,12 +12,13 @@ class SeekBarActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_seek_bar)
+        //setContentView(R.layout.activity_seek_bar)
 
-        prange_seekbar.setOnRangeSeekbarChangeListener { minValue, maxValue ->
+        // not relevant for release at the moment  produces error
+        /*prange_seekbar.setOnRangeSeekbarChangeListener { minValue, maxValue ->
             price_range.setText("$minValue -- $maxValue")
 
-        }
+        }*/
 
     }
 }
