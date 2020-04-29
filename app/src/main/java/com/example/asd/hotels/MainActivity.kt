7164d40d0a20cd6b26577f96ba307149c8acfafc
Sandra Haas <sandra.michaela.haas@gmail.com>
2020-04-29 15:53:26 +0200

    HOTEL_001 Overview #1 removed message button [MH, SH]

--- app/src/main/java/com/example/asd/hotels/MainActivity.kt
+++ app/src/main/java/com/example/asd/hotels/MainActivity.kt
@@ -20,13 +20,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         //setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()*/
-            val intent = Intent(this, HotelDetailActivity::class.java)
-            startActivity(intent)
-        }
-
         val hotel_values = listOf(
             HotelData(1, 1, "Vienna", R.drawable.untitled,
                 "Sample Name", 500, 200,
