ec4de5c26b7a4c2d98463f11bd064fa86b1a7309
Geoetrev <johanneswind57@gmail.com>
2020-05-07 16:44:00 +0200

    HOTEL_008A Sorting for Stars and Rating [HS, MC, PJ]

--- app/src/main/java/com/example/asd/hotels/MainActivity.kt
+++ app/src/main/java/com/example/asd/hotels/MainActivity.kt
@@ -20,38 +20,68 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
         setSupportActionBar(toolbar)
 
-
-        val hotel_values = listOf(
-            HotelData(1, 1, "Graz", R.drawable.untitled, "Alpha", 3, 3,"Gutes Hotel!"),
-            HotelData(2, 2, "Graz", R.drawable.untitled, "Beta", 2, 2, "Gutes Hotel!"),
-            HotelData(3, 1, "Graz", R.drawable.untitled, "Charlie", 7, 7, "Gutes Hotel!"),
-            HotelData(4, 1, "Graz", R.drawable.untitled, "Delta", 14, 9, "Gutes Hotel!"),
-            HotelData(5, 1, "Graz", R.drawable.untitled, "Echo", 9, 12, "Gutes Hotel!"),
-            HotelData(6, 1, "Graz", R.drawable.untitled, "Foxtrot", 1, 1, "Gutes Hotel!"),
-            HotelData(7, 1, "Graz", R.drawable.untitled,  "Golf", 1, 2, "Gutes Hotel!"),
-            HotelData(8, 1, "Graz", R.drawable.untitled, "Hotel", 3, 3, "Gutes Hotel!"),
-            HotelData(9, 1, "Graz", R.drawable.untitled, "India", 4, 4, "Gutes Hotel!")
-        )
+        val hotelValues = mutableListOf<HotelData>();
+        var idCounter = 0
+        var hotelNames = listOf("Alpha", "Beta", "Charlie", "Delta", "Echo", "Foxtrot", "Golf", "Hotel", "India")
+        for (i in 0..10) {
+            hotelValues.add(HotelData(idCounter, 1, "Graz", R.drawable.untitled, hotelNames.get((0..9).random()), (0..100).random(), (0..100).random(), "Gutes Hotel!", (0..10).random(), (1..5).random()))
+            idCounter++
+        }
 
 
         var overViewVisible = true
         sort_button_price.setOnClickListener {
-
             //add fragment to fill sorting view
             //supportFragmentManager.beginTransaction()
             //    .add(R.id.SortingView, MainFragment.newInstance(), "hotelList").commit()
 
-
-            var hotelDummyListSorted = hotel_values.sortedBy { it.price }
-
+            var hotelDummyListSorted = hotelValues.sortedBy { it.price }
             SortingView.apply {
                 layoutManager = LinearLayoutManager(this@MainActivity)
                 adapter = SortingViewAdapter(hotelDummyListSorted)
             }
+            if (overViewVisible) {
+                OverView.setVisibility(View.INVISIBLE)
+                SortingView.setVisibility(View.VISIBLE)
+                overViewVisible = false
+            } else {
+                OverView.setVisibility(View.VISIBLE)
+                SortingView.setVisibility(View.GONE)
+                overViewVisible = true
+            }
+        }
+        sort_button_rating.setOnClickListener {
+            //add fragment to fill sorting view
+            //supportFragmentManager.beginTransaction()
+            //    .add(R.id.SortingView, MainFragment.newInstance(), "hotelList").commit()
 
+            var hotelDummyListSorted = hotelValues.sortedBy { it.hotel_rating }
+            SortingView.apply {
+                layoutManager = LinearLayoutManager(this@MainActivity)
+                adapter = SortingViewAdapter(hotelDummyListSorted)
+            }
+            if (overViewVisible) {
+                OverView.setVisibility(View.INVISIBLE)
+                SortingView.setVisibility(View.VISIBLE)
+                overViewVisible = false
+            } else {
+                OverView.setVisibility(View.VISIBLE)
+                SortingView.setVisibility(View.GONE)
+                overViewVisible = true
+            }
+        }
+        sort_button_stars.setOnClickListener {
+            //add fragment to fill sorting view
+            //supportFragmentManager.beginTransaction()
+            //    .add(R.id.SortingView, MainFragment.newInstance(), "hotelList").commit()
+
+            var hotelDummyListSorted = hotelValues.sortedBy { it.hotel_stars }
+            SortingView.apply {
+                layoutManager = LinearLayoutManager(this@MainActivity)
+                adapter = SortingViewAdapter(hotelDummyListSorted)
+            }
             if (overViewVisible) {
                 OverView.setVisibility(View.INVISIBLE)
                 SortingView.setVisibility(View.VISIBLE)
@@ -63,16 +93,21 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+
         // Setting up the adapter
         OverView.apply {
             // Set up the layer
             layoutManager = LinearLayoutManager(this@MainActivity)
             // Pass the list into OverViewAdapter
-            adapter = OverViewAdapter(hotel_values) {
-                d("MainActivity","Hi from main")
+            adapter = OverViewAdapter(hotelValues) {
+                d("MainActivity", "Hi from main")
                 // Call the detail view.
-                startActivity(Intent(this@MainActivity,
-                    HotelDetailActivity::class.java))
+                startActivity(
+                    Intent(
+                        this@MainActivity,
+                        HotelDetailActivity::class.java
+                    )
+                )
             }
         }
 
