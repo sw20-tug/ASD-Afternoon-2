3b927d16aea9762f37ce25f91751e602106ca768
Geoetrev <johanneswind57@gmail.com>
2020-05-20 13:50:36 +0200

    HOTEL_008C Set up Test File

---
+++ app/src/androidTest/java/com/example/asd/hotels/SortingViewAdapterTest.kt
@@ -0,0 +1,30 @@
+package com.example.asd.hotels
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+public class SortingViewAdapterTest(private var adapter: SortingViewAdapter) {
+
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        // val appContext = InstrumentationRegistry.getTargetContext()
+        //assertEquals("com.example.asd.hotels", appContext.packageName)
+    }
+}
