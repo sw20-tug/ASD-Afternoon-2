f44444dd8d940b151824d38faa2c939431008632
Sandra Haas <sandra.michaela.haas@gmail.com>
2020-05-27 18:36:32 +0200

    HOTEL_009F extended Detail view testcases [HS, SB]

--- app/src/androidTest/java/com/example/asd/hotels/HotelDetailActivityTest.kt
+++ app/src/androidTest/java/com/example/asd/hotels/HotelDetailActivityTest.kt
@@ -1,5 +1,6 @@
 package com.example.asd.hotels
 
+import android.content.Intent
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -10,10 +11,14 @@ import androidx.test.espresso.intent.rule.IntentsTestRule
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.ActivityTestRule
+import com.example.asd.hotels.dummy.HotelData
 import org.hamcrest.Matchers.allOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+
 
 private const val PACKAGE_NAME = "com.example.asd.hotels"
 
@@ -21,13 +26,32 @@ private const val PACKAGE_NAME = "com.example.asd.hotels"
 
 @RunWith(AndroidJUnit4::class)
 class HotelDetailActivityTest {
-    @Rule
-    @JvmField
-    var activityRule = IntentsTestRule<HotelDetailActivity>(
-        HotelDetailActivity::class.java
-    )
+    var hotelData: HotelData = HotelData(1, 2, "there",  R.drawable.untitled, "Hotel SUPER", 200, 20, "Nice hotel", 5, 4)
 
-    private var hotel_id = -1
+    @get:Rule
+    val mActivityTestRule: IntentsTestRule<HotelDetailActivity> =
+        object : IntentsTestRule<HotelDetailActivity>(HotelDetailActivity::class.java) {
+            override fun getActivityIntent(): Intent {
+                val targetContext = InstrumentationRegistry.getInstrumentation().targetContext
+                return Intent(targetContext, HotelDetailActivity::class.java).apply {
+                    putExtra("hotelData", hotelData)
+                }
+            }
+        }
+
+    @Test
+    fun verifyIfRatingBarIsCorrect() {
+        // check if rating bar exists
+        onView(withId(R.id.ratingBar)).check(matches(isDisplayed()))
+        // todo check if rating is correctly set
+    }
+
+    @Test
+    fun verifyIfHotelStarsAreCorrect() {
+        // check if rating bar exists
+        onView(withId(R.id.hotelStars)).check(matches(isDisplayed()))
+        // todo check if stars are correctly set
+    }
 
     @Test
     fun verifyIfHotelRatingOpens() {
@@ -36,12 +60,12 @@ class HotelDetailActivityTest {
         // Clicks rating_button
         onView(withId(R.id.rating_button)).perform(click())
 
-        // Verifies that the HotelRatingActivity received an intent
+        // Verifies that the HotelRatingActivity received an intent with the correct id
         // with the correct package name and message.
         intended(allOf(
             hasComponent(hasShortClassName(".HotelRatingActivity")),
             toPackage(PACKAGE_NAME),
-            hasExtra("hotel_id", hotel_id)))
+            hasExtra("hotel_id", hotelData.hotel_id)))
     }
 
 }
