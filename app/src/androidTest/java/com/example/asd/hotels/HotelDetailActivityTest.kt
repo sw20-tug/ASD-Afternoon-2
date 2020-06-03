3776e27a5d71bd390ee8c2396d33739022e8adc7
Sandra Haas <sandra.michaela.haas@gmail.com>
2020-06-03 13:41:41 +0200

    HOTEL_009F added fragment tests [HS, RM]

--- app/src/androidTest/java/com/example/asd/hotels/HotelDetailActivityTest.kt
+++ app/src/androidTest/java/com/example/asd/hotels/HotelDetailActivityTest.kt
@@ -1,6 +1,8 @@
 package com.example.asd.hotels
 
 import android.content.Intent
+import android.os.Bundle
+import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
@@ -8,16 +10,16 @@ import androidx.test.espresso.intent.Intents.intended
 import androidx.test.espresso.intent.matcher.ComponentNameMatchers.hasShortClassName
 import androidx.test.espresso.intent.matcher.IntentMatchers.*
 import androidx.test.espresso.intent.rule.IntentsTestRule
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.rule.ActivityTestRule
 import com.example.asd.hotels.dummy.HotelData
 import org.hamcrest.Matchers.allOf
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import androidx.fragment.app.testing.launchFragmentInContainer as launchFragmentInContainer1
 
 
 private const val PACKAGE_NAME = "com.example.asd.hotels"
@@ -26,7 +28,7 @@ private const val PACKAGE_NAME = "com.example.asd.hotels"
 
 @RunWith(AndroidJUnit4::class)
 class HotelDetailActivityTest {
-    var hotelData: HotelData = HotelData(1, 2, "there",  R.drawable.untitled, "Hotel SUPER", 200, 20, "Nice hotel", 5, 4)
+    var hotelData: HotelData = HotelData(1, 2, "there",  R.drawable.untitled, "Hotel SUPER", 200, 20, "Nice hotel", 5.0F, 4)
 
     @get:Rule
     val mActivityTestRule: IntentsTestRule<HotelDetailActivity> =
@@ -51,6 +53,41 @@ class HotelDetailActivityTest {
         // check if rating bar exists
         onView(withId(R.id.hotelStars)).check(matches(isDisplayed()))
         // todo check if stars are correctly set
+    }
+    @Test fun testPictureFragment() {
+        // check if hotel pictures are displayed
+        val fragment = HotelPictureFragment().apply {
+            arguments = Bundle().apply {
+                putParcelable(HotelDetailFragment.ARG_ITEM,
+                    hotelData)
+                putInt(
+                    HotelDetailFragment.ARG_ITEM_ID,
+                    hotelData.hotel_id
+                )
+            }
+        }
+        mActivityTestRule.getActivity().getSupportFragmentManager().beginTransaction()
+            .add(R.id.hotel_picture_container, fragment).commit()
+
+        //onView(withId(R.id.PhotoView)).check(matches(isDisplayed()))
+    }
+
+    @Test fun testDetailFragment() {
+        // check if hotel pictures are displayed
+        val fragment = HotelDetailFragment().apply {
+            arguments = Bundle().apply {
+                putParcelable(HotelDetailFragment.ARG_ITEM,
+                    hotelData)
+                putInt(
+                    HotelDetailFragment.ARG_ITEM_ID,
+                    hotelData.hotel_id
+                )
+            }
+        }
+
+        mActivityTestRule.getActivity().getSupportFragmentManager().beginTransaction()
+            .add(R.id.hotel_detail_container, fragment).commit()
+
     }
 
     @Test
